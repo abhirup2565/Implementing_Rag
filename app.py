4d0b046afc332cd7891ffe33b3b07c6cb0167ffb
abhirup2565 <abhirup2565@gmail.com>
2025-10-01 21:30:40 +0530

    Basic Implementaion of rag

---
+++ app.py
@@ -0,0 +1,98 @@
+from langchain_community.document_loaders import HuggingFaceDatasetLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import AutoTokenizer, pipeline
+from langchain import HuggingFacePipeline
+from langchain.chains import RetrievalQA
+
+#-----------Document Loading-----------------
+# Specify the dataset name and the column containing the content
+dataset_name = "databricks/databricks-dolly-15k"
+page_content_column = "context"  # or any other column you're interested in
+
+# Create a loader instance
+loader = HuggingFaceDatasetLoader(dataset_name, page_content_column)
+
+# Load the data
+data = loader.load()
+
+# Display the first 15 entries 
+data[:2] #2 represent the coloum from dataset
+
+#------------Document Transformer-----------------
+# Create an instance of the RecursiveCharacterTextSplitter class with specific parameters.
+# It splits text into chunks of 1000 characters each with a 150-character overlap.
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+
+# 'data' holds the text you want to split, split the text into documents using the text splitter.
+docs = text_splitter.split_documents(data)
+
+print(docs[0])  # Print the first document chunk to verify the splitting
+
+#------------Text Embedding-----------------
+# Define the path to the pre-trained model you want to use
+modelPath = "sentence-transformers/all-MiniLM-l6-v2"
+
+# Create a dictionary with model configuration options, specifying to use the CPU for computations
+model_kwargs = {'device':'cpu'}
+
+# Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
+encode_kwargs = {'normalize_embeddings': False}
+
+# Initialize an instance of HuggingFaceEmbeddings with the specified parameters
+embeddings = HuggingFaceEmbeddings(
+    model_name=modelPath,     # Provide the pre-trained model's path
+    model_kwargs=model_kwargs, # Pass the model configuration options
+    encode_kwargs=encode_kwargs # Pass the encoding options
+)
+
+#Test the embedding model with a sample text
+text = "This is a test document."
+query_result = embeddings.embed_query(text)
+query_result[:3]# Print the first 3 values of the resulting embedding vector
+
+#------------Vector Store-----------------
+db = FAISS.from_documents(docs, embeddings)# Create a FAISS vector store from the documents and their embeddings
+
+# Test the vector store with a sample query
+question = "What is cheesemaking?"
+searchDocs = db.similarity_search(question)
+print(searchDocs[0].page_content)
+
+#------------Preparing langchain Pipeline -----------------
+# Specify the model name you want to use
+model_name = "Intel/dynamic_tinybert"
+
+# Load the tokenizer associated with the specified model
+tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=512)
+
+# Define a question-answering pipeline using the model and tokenizer
+question_answerer = pipeline(
+    "question-answering", 
+    model=model_name, 
+    tokenizer=tokenizer,
+    return_tensors='pt'
+)
+
+# Create an instance of the HuggingFacePipeline, which wraps the question-answering pipeline
+# with additional model-specific arguments (temperature and max_length)
+llm = HuggingFacePipeline(
+    pipeline=question_answerer,
+    model_kwargs={"temperature": 0.7, "max_length": 512},
+)
+
+#------------Creating Retreiver -----------------
+# Create a retriever object from the 'db' using the 'as_retriever' method.
+# This retriever is likely used for retrieving data or documents from the database.
+retriever = db.as_retriever(search_kwargs={"k": 4})
+
+# Create a question-answering instance (qa) using the RetrievalQA class.
+# It's configured with a language model (llm), a chain type "refine," the retriever we created, and an option to not return source documents.
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="refine", retriever=retriever, return_source_documents=False)
+
+#------------Test the pipeline -----------------
+question = "Who is Thomas Jefferson?"
+result = qa.run({"query": question})
+print(result["result"])
